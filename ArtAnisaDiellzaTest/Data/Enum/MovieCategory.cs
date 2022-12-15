a9fa4891ce4487213bd79b0f56a191c176eb274e
artshabani2002 <artshabani2002@gmail.com>
2022-12-15 21:52:01 +0100

    Kriji i DbContext dhe modelit Movie

---
+++ ArtAnisaDiellzaTest/Data/Enum/MovieCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace ArtAnisaDiellzaTest.Controllers
+{
+	public enum MovieCategory
+	{
+
+		Action = 1,
+		Comedy,
+		Drama,
+		Romance,
+		Horror,
+		Animated,
+		Documentary,
+		Fantasy,
+		Fiction,
+		Crime,
+		Thriller,
+
+
+
+
+	}
+}
---
+++ ArtAnisaDiellzaTest/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ArtAnisaDiellzaTest.Models;
+
+using ArtAnisaDiellzaTest.Controllers;
+using System.Windows;
+
+
+
+public class Movie
+{
+
+	public int MovieID { get; set; }
+
+	public string Name { get; set; }
+
+	public string Description { get; set; }
+
+	public string ImageURL { get; set; }
+
+	public int Duration { get; set; }
+
+	public DateTime ReleaseDate { get; set; }
+
+	public MovieCategory MovieCategory { get; set; }
+
+
+
+
+
+
+
+}
