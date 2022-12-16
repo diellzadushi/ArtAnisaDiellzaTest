dd57acf67edd7f965f4fa2122752d8428bcb5581
artshabani2002 <artshabani2002@gmail.com>
2022-12-16 23:38:23 +0100

    lidhja e db-s, ndrrone emrin e serverit, bonja add-migration edhe update-database,

--- ArtAnisaDiellzaTest/Data/AppDbContext.cs
+++ ArtAnisaDiellzaTest/Data/AppDbContext.cs
@@ -1,15 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ArtAnisaDiellzaTest.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArtAnisaDiellzaTest.Data
 {
 	public class AppDbContext : DbContext
 	{
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 		{
 
 
 		}
 
+
+		public DbSet<Movie> Movies { get; set; }
 	}
 }
---
+++ ArtAnisaDiellzaTest/Migrations/20221216222850_initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtAnisaDiellzaTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MovieCategory = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
