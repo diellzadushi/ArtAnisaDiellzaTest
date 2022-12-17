d8b25f86ec702ac4d06bc8a17780755764ce7857
Diellza Dushi <dd51383@ubt-uni.net>
2022-12-17 15:30:42 +0100

    Shtimi i Controllerit Movies

---
+++ ArtAnisaDiellzaTest/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using ArtAnisaDiellzaTest.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtAnisaDiellzaTest.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MovieController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allMovies = await _context.Movies.ToListAsync();
+            return View(allMovies);
+        }
+    }
+}
