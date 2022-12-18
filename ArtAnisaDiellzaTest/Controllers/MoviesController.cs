aa505e8a9b32f15cfab6975ed6a0f2dea0f93b14
artshabani2002 <artshabani2002@gmail.com>
2022-12-18 22:36:12 +0100

    movie-view-added

--- ArtAnisaDiellzaTest/Controllers/MoviesController.cs
+++ ArtAnisaDiellzaTest/Controllers/MoviesController.cs
@@ -16,7 +16,7 @@ namespace ArtAnisaDiellzaTest.Controllers
         public async Task<IActionResult> Index()
         {
             var data = await _context.Movies.ToListAsync();
-            return View();
+            return View(data);
         }
     }
 }
